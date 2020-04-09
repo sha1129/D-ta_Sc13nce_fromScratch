edf86ef57615ce89ac2ae52a4eef8433b775f0ef
sha1129 <63379238+sha1129@users.noreply.github.com>
2020-04-08 20:36:58 -0400

    Add files via upload

---
+++ startingAgain.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 15:59:53 2020
+
+@author: turjo
+"""
+
+#white space formating 
+#indentation 
+for i in [1,2,3,4,5]:
+    print("亲亲", i)
+    for j in [i]:
+        print("bottom loop",i+1)
+    print(i, "i love you")
+print("亲亲")
+
+#for if
+for i in [1,2,3,4,5]:
+    if i ==5:
+        print ("5")
+    else:
+        print (i)
+    
+    
+#white space ignored in long cal
+
+longCal = (1+22222-4-5+555+56+56666666666666666+
+    7+8)
+
+#backlast to indicate stament in next page \
+ 
+blah = 1 *\
+    2
+
+
+    
